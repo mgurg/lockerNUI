1350efa0fd3c81307eb920cd82fbcb4bd534bbf5
mgurg <34517378+mgurg@users.noreply.github.com>
2024-10-07 14:02:09 +0200

    Plain index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="max-w-7xl mx-auto">
-    <h1 class="page-title py-4"> Najlepsze Escape Roomy! </h1>
+    <h1 class="page-title py-4">Escape room w Polsce</h1>
 
     <VaCard class="mb-4">
       <VaCardContent>
-        <h1 class="text-2xl font-bold leading-5 mb-4">Przeglądaj Escape Roomy w Polsce</h1>
+        <h1 class="text-2xl font-bold leading-5 mb-4">Analiza istniejących biznesów w Polsce</h1>
         <div class="text-base">
-          Witaj na naszej stronie, która stanowi przewodnik po najlepszych escape roomach w Polsce!
+          Nic tu jeszcze nie ma 🥺
         </div>
-
-
+        <VaButton class="mt-6">Zajrzyj na blog</VaButton>
       </VaCardContent>
     </VaCard>
   </div>
