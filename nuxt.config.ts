ee1e195c60f606aa598c479fa125670e069617a3
mgurg <34517378+mgurg@users.noreply.github.com>
2024-10-15 15:34:18 +0200

    Remove vuestic leftovers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    modules: ['@nuxt/ui'],
+    future: {
+        compatibilityVersion: 4,
     },
-  },
-  
-  modules: [
-    '@vuestic/nuxt'
-  ],
-
-  vuestic: {
-    config: {
-      // Vuestic config here
-    },
-    
-    css: ['typography'],
-  },
-
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true}
 })
