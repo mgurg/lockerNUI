c703417bf461edc87fb0d450fa29e0d74f3cbdfa
mgurg <34517378+mgurg@users.noreply.github.com>
2024-10-31 09:54:49 +0100

    Introduce env support

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,26 +1,18 @@
 <template>
-  <!--  <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl gap-16 sm:gap-y-24 grid lg:grid-cols-2 lg:items-center">-->
-
   <UContainer>
     <UCard>
       <template #header>
         <h1 class="text-4xl font-extrabold">Analiza rynku Escape Room w Polsce</h1>
       </template>
-
-      Zobacz listę firm, lub zajrzyj na blog
+      Nic tu jeszcze nie ma 🫣 ale na <ULink  :href="blogUrl">blogu</ULink> są już pierwsze artukuły 🙂
     </UCard>
   </UContainer>
-
-
-  <!--  </div>-->
 </template>
 
-
 <script setup lang="ts">
-
+const { public: { BLOG_URL } } = useRuntimeConfig();
+const blogUrl = BLOG_URL;
 </script>
 
-
 <style scoped>
-
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ['@nuxt/ui'],
+    runtimeConfig: {
+        public: {
+          BLOG_URL: process.env.BLOG_URL || 'https://default-blog-url.com',
+        },
+      },
     future: {
         compatibilityVersion: 4,
     },
