32beada54452bf14ed62e4c79e5fe5e48d4c441c
mgurg <34517378+mgurg@users.noreply.github.com>
2024-12-06 13:05:39 +0100

    Static companies list

--- app/pages/companies.vue
+++ app/pages/companies.vue
@@ -1,91 +1,33 @@
 <template>
-  <div class="flex flex-col min-h-screen">
-    <!-- Main content -->
-    <div class="flex-grow">
-      <div v-for="(item, index) in items"
-           :key="index">
-        <UCard>
-          {{ item.label }}
-        </UCard>
-      </div>
-      <div v-if="loading">Loading data...</div>
-    </div>
 
-    <!-- Pagination -->
-    <div class="w-full">
-      <div class="flex justify-center items-center py-4">
-        <UPagination
-            v-if="!loading"
-            v-model:page="page"
-            :total="totalItems"
-            :page-count="Math.ceil(totalItems / itemsPerPage)"
-            :to="to"
-            :sibling-count="1"
-            show-edges
-        />
-      </div>
-    </div>
-  </div>
+  <UContainer>
+    <UAccordion :items="items"/>
+  </UContainer>
+
+
 </template>
 
 
-
-
 <script setup lang="ts">
-import {ref, watch} from "vue";
-import {CompanyService} from "@/client";
-
-// https://github.com/Semolik/music/blob/3aa04a1ed3efae9e57d9c71942f959655c1d085a/frontend/stores/auth.js#L51
-
-
-// Pagination state
-const items = ref([]); // Accordion items
-const page = ref(1); // Current page
-const totalItems = ref(0); // Total number of items
-const itemsPerPage = 10; // Items per page (same as API's `limit`)
-const loading = ref(true); // Loading state
-
-// Define the `to` function for pagination links
-function to(page: number) {
-  return {
-    query: {
-      page,
-    },
-  };
-}
-
-// Fetch data for the current page
-async function fetchPageData() {
-  loading.value = true; // Start loading
-  try {
-    const offset = (page.value - 1) * itemsPerPage;
-
-    // Fetch data from the API
-    const response = await CompanyService.getCompaniesCompaniesGet(
-        null, // Search query (optional)
-        itemsPerPage,
-        offset
-    );
-
-    // Update total items and items
-    totalItems.value = response.count;
-    items.value = response.data.map((company) => ({
-      label: company.name,
-      icon: "heroicons:building-office",
-      content: company.name,
-    }));
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  } finally {
-    loading.value = false; // End loading
+const items = ref([
+  {
+    label: 'ER Corp',
+    icon: 'heroicons:building-office',
+    content: 'ER Corp'
+  },
+  {
+    label: 'ER ACME',
+    icon: 'heroicons:building-office',
+    content: 'ER ACME'
+  },
+  {
+    label: 'Ermex-pol',
+    icon: 'heroicons:building-office',
+    content: 'Ermex-pol'
   }
-}
-
-// Watch for changes to the `page` and fetch data accordingly
-watch(page, fetchPageData);
-
-// Initial fetch
-fetchPageData();
+])
 </script>
 
+<style scoped>
 
+</style>
