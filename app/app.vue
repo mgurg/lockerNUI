ae6413cd08819e940ae003cd600137d0685d2181
mgurg <34517378+mgurg@users.noreply.github.com>
2025-05-20 15:16:00 +0200

    Bing Webmaster tools verification

--- app/app.vue
+++ app/app.vue
@@ -10,5 +10,14 @@
 @import "tailwindcss";
 @import "@nuxt/ui";
 </style>
+
 <script setup lang="ts">
+useHead({
+  meta: [
+    {
+      name: 'msvalidate.01',
+      content: '9C7D5BBDE799324043D7F0AF819431EE'
+    }
+  ]
+})
 </script>
